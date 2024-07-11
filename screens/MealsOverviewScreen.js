3db8d03d12728c1d586aa9e09e9ee03a4bcaed43
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-07-11 17:13:35 +0530

    Add title to navigated menu item screen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,25 @@
 import { FlatList, StyleSheet, Text, View } from "react-native"
-import { MEALS } from '../data/dummy-data'
+import { CATEGORIES, MEALS } from '../data/dummy-data'
 import MealItem from "../components/MealItem";
+import { useEffect } from "react";
 
-const MealsOverviewScreen = ({ route }) => {
+const MealsOverviewScreen = ({ route, navigation }) => {
 
   const catId = route.params.categoryId;
 
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catId) >= 0;
   });
+
+  useEffect(() => {
+    const categoryTitle = CATEGORIES.find(
+      (category) => category.id === catId
+    ).title;
+
+    navigation.setOptions({
+      title: categoryTitle,
+    })
+  }, [catId, navigation])
 
   const renderMealItem = (itemData) => {
 
